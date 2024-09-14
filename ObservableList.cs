544e38f1ba4f3d187ba320833522a18a7d7361c3
Minnator <david@schlensok.de>
2024-09-14 03:04:30 +0200

    Added an obeservable list but decided not to use it

---
+++ ObservableList.cs
@@ -0,0 +1,21 @@
+﻿namespace Editor
+{
+   public class ObservableList<T>
+   {
+      private readonly List<T> _list = [];
+
+      public event Action<T>? ItemAdded;
+
+      public void Add(T item)
+      {
+         _list.Add(item);
+         OnItemAdded(item); 
+      }
+
+      protected virtual void OnItemAdded(T item)
+      {
+         ItemAdded?.Invoke(item);
+      }
+
+   }
+}
